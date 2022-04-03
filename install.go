0dc01d349bebbe1525545b6f143ff0dc91be0937
lawl <github@dumbinter.net>
2022-04-03 14:22:04 +0200

    CLI: print ERROR/WARNING/INFO in color

--- install.go
+++ install.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/fs"
 	"os"
 	"strings"
 )
@@ -12,5 +13,20 @@ func installAppimage(path string) {
 		os.Exit(1)
 	}
 	ai := ai(path)
-	_ = ai
+
+	matches, err := fs.Glob(ai.FS, "*.desktop")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: Cannot glob for desktop file: %s\n", err)
+		os.Exit(1)
+	}
+
+	if len(matches) == 0 {
+		fmt.Fprintf(os.Stderr, "Error: AppImage does not contain a desktop file. Integration for desktop-file less images is not supported yet.")
+		os.Exit(1)
+	}
+	internalDesktopFilePath := matches[0]
+	if len(matches) > 1 {
+		fmt.Fprintf(os.Stderr, "Warning: Multiple .desktop files found in AppImage root, using '%s'\n", internalDesktopFilePath)
+	}
+
 }
--- main.go
+++ main.go
@@ -2,11 +2,25 @@ package main
 
 import (
 	"ayy/appimage"
+	"ayy/fancy"
 	"flag"
 	"fmt"
 	"os"
 	"strings"
 )
+
+var WARNING string
+var ERROR string
+var INFO string
+
+func init() {
+	fp := fancy.Print{}
+	WARNING = fp.Bold().Color(fancy.Yellow).Format("WARNING: ")
+	fp = fancy.Print{}
+	ERROR = fp.Bold().Color(fancy.Red).Format("ERROR: ")
+	fp = fancy.Print{}
+	INFO = fp.Bold().Color(fancy.Cyan).Format("INFO: ")
+}
 
 func main() {
 	if len(os.Args) < 2 {
@@ -23,11 +37,11 @@ func main() {
 		ai := ai(os.Args[2])
 		updInfo, err := ai.UpdateInfo()
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error reading update info: %s\n", err)
+			fmt.Fprintf(os.Stderr, ERROR+"reading update info: %s\n", err)
 		}
 		sha256sig, err := ai.Sha256Sig()
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error reading update info: %s\n", err)
+			fmt.Fprintf(os.Stderr, ERROR+"reading update info: %s\n", err)
 		}
 		fmt.Printf("Update: %s\n", updInfo)
 
@@ -35,7 +49,6 @@ func main() {
 		//there may also be better signature schemes one could implement
 		fmt.Printf("SHA256 sig: %s\n", string(sha256sig))
 	case "fs":
-
 		if len(os.Args) < 4 {
 			fmt.Fprintf(os.Stderr, "TODO Usage '%s fs /foo/bar.AppImage subcommand'\n", os.Args[0])
 			os.Exit(1)
@@ -46,7 +59,7 @@ func main() {
 			ls := flag.NewFlagSet("ls", flag.ExitOnError)
 			usebytes := ls.Bool("b", false, "Display sizes in bytes instead of human readable string")
 			if err := ls.Parse(os.Args[4:]); err != nil {
-				fmt.Fprintf(os.Stderr, "Unable to parse flags: %s\n", err)
+				fmt.Fprintf(os.Stderr, ERROR+"Unable to parse flags: %s\n", err)
 				os.Exit(1)
 			}
 			listFiles(os.Args[2], ls.Arg(0), *usebytes)
@@ -54,7 +67,7 @@ func main() {
 		case "cat":
 			cat := flag.NewFlagSet("cat", flag.ExitOnError)
 			if err := cat.Parse(os.Args[4:]); err != nil {
-				fmt.Fprintf(os.Stderr, "Unable to parse flags: %s\n", err)
+				fmt.Fprintf(os.Stderr, ERROR+"Unable to parse flags: %s\n", err)
 				os.Exit(1)
 			}
 			catFile(os.Args[2], cat.Arg(0))
@@ -85,7 +98,7 @@ func globalHelp() {
 func ai(path string) *appimage.AppImage {
 	app, err := appimage.NewAppImage(path)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Couldn't open AppImage: %s\n", err)
+		fmt.Fprintf(os.Stderr, ERROR+"Couldn't open AppImage: %s\n", err)
 		os.Exit(1)
 	}
 	return app
