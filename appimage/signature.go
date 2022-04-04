26ecd1d6f59910263addc345444284d2bb301de3
lawl <github@dumbinter.net>
2022-04-04 15:15:53 +0200

    appimage: Add docs to SHA256WithoutSignature() function

--- appimage/signature.go
+++ appimage/signature.go
@@ -43,6 +43,10 @@ func (ai *AppImage) Signature() (signedby openpgp.Entity, ok bool, err error) {
 	return *entity, true, nil
 }
 
+// SHA256WithoutSignature computes the SHA256 sum of the AppImage
+// but treats the ELF sections .sha256_sig and .sig_key as if
+// they were entirely 0x00 bytes. That's because this hash
+// is later signed and stuffed into exactly these sections
 func (ai *AppImage) SHA256WithoutSignature() ([]byte, error) {
 	if _, err := ai.file.Seek(0, io.SeekStart); err != nil {
 		fmt.Println(err)
